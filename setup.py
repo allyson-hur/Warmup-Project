b2799c142e6d39a733f352c9a7e5c1af404a1262
evlock <elockwood@olin.edu>
2022-09-24 20:35:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,8 @@ setup(
             'drive_square= Warmup-Project.drive_square:main',
             'wall_follower=Warmup-Project.wall_follower:main',
             'person_follower=Warmup-Project.person_follower:main',
-            'obstacle_avoider=Warmup-Project.obstacle_avoider:main'
+            'obstacle_avoider=Warmup-Project.obstacle_avoider:main',
+            'finite_state_controller=Warmup-Project.finite_state_controller:main'
         ],
     },
 )
